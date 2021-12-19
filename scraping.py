1bf522bfb775e7c6d3e4d66811920afe5ba58690
Michal Michael <michalmichael@Michals-iMac.lan>
2021-12-19 12:25:25 -0500

    added scraping.py

--- scraping.py
+++ scraping.py
@@ -1,80 +1,71 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# Import Splinter and BeautifulSoup
+# Import Splinter, BeautifulSoup, and Pandas
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
-from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
+from webdriver_manager.chrome import ChromeDriverManager
 
+## Set up Splinter
 executable_path = {'executable_path': ChromeDriverManager().install()}
 browser = Browser('chrome', **executable_path, headless=False)
 
-# Visit the mars nasa news site
-url = 'https://redplanetscience.com'
+
+# Visit the Mars news site
+url = 'https://redplanetscience.com/'
 browser.visit(url)
+
+# Optional delay for loading the page
 browser.is_element_present_by_css('div.list_text', wait_time=1)
 
+# Convert the browser html to a soup object and then quit the browser
 html = browser.html
 news_soup = soup(html, 'html.parser')
+
 slide_elem = news_soup.select_one('div.list_text')
 
-
-# In[6]:
 slide_elem.find('div', class_='content_title')
 
-
-# In[7]:
+# Use the parent element to find the first a tag and save it as `news_title`
 news_title = slide_elem.find('div', class_='content_title').get_text()
 news_title
 
-
-# In[8]:
+# Use the parent element to find the paragraph text
 news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
 news_p
 
+# ## JPL Space Images Featured Image
 
-# ### Featured Images
-
-# In[14]:
+# Visit URL
 url = 'https://spaceimages-mars.com'
 browser.visit(url)
 
-
-# In[15]:
+# Find and click the full image button
 full_image_elem = browser.find_by_tag('button')[1]
 full_image_elem.click()
 
-
-# In[16]:
+# Parse the resulting html with soup
 html = browser.html
 img_soup = soup(html, 'html.parser')
 
-
-# In[17]:
-# Find the relative image url
+# find the relative image url
 img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
 img_url_rel
 
-
-# In[18]:
+# Use the base url to create an absolute url
 img_url = f'https://spaceimages-mars.com/{img_url_rel}'
 img_url
 
+# ## Mars Facts
 
-# In[20]:
 df = pd.read_html('https://galaxyfacts-mars.com')[0]
-df.columns=['description', 'Mars', 'Earth']
-df.set_index('description', inplace=True)
+df.head()
+
+df.columns=['Description', 'Mars', 'Earth']
+df.set_index('Description', inplace=True)
 df
 
-
-# In[21]:
 df.to_html()
 
-
-# In[22]:
 browser.quit()
-
-
-

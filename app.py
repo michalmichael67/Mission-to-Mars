6b8f0d615b66815ae23ef7b54ca05143486f8eff
Michal Michael <michalmichael@Michals-iMac.lan>
2021-12-19 12:30:06 -0500

    modules 10.5.1

--- app.py
+++ app.py
@@ -10,14 +10,16 @@ mongo = PyMongo(app)
 
 @app.route("/")
 def index():
-    mars = mongo.db.mars.find_one()
-    return render_template("index.html", mars=mars)
+   mars = mongo.db.mars.find_one()
+   return render_template("index.html", mars=mars)
+
+
 @app.route("/scrape")
 def scrape():
-    mars = monngo.db.mars
-    mars_data = scraping.scrape_all()
-    mars.update({}, mars_data, upsert=True)
-    return redirect('/', code=302)
+   mars = mongo.db.mars
+   mars_data = scraping.scrape_all()
+   mars.update({}, mars_data, upsert=True)
+   return redirect('/', code=302)
 
 
 if __name__ == "__main__":
